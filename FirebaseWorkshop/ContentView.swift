669947c416311338ecd24d83a0b5b0867dec1075
David Estrella <dtestrella@ucdavis.edu>
2026-02-02 20:39:44 -0800

    Initial Commit

---
+++ FirebaseWorkshop/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FirebaseWorkshop
+//
+//  Created by David Estrella on 2/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FirebaseWorkshop/FirebaseWorkshopApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseWorkshopApp.swift
+//  FirebaseWorkshop
+//
+//  Created by David Estrella on 2/2/26.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseWorkshopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
